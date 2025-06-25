8c56924cf0209006e015c8831394a936c4bdd7b1
bjaral <benjaminjara1505@gmail.com>
2025-06-25 05:00:34 -0400

    feat(fontend): mecanismo de listas

--- frontend/src/app/components/test/test.component.ts
+++ frontend/src/app/components/test/test.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { SensorService } from '../../services/sensor.service';
+import { HistoricosService, Humedad, Temperatura, Flujo } from '../../services/historicos.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
@@ -13,17 +15,61 @@ export class TESTComponent {
   datoHumedadReal = "";
   datoFlujoReal = "";
 
-  constructor(private sensorService: SensorService) { }
+  listaHumedad: Humedad[] = [];
+  listaTemperatura: Temperatura[] = [];
+  listaFlujo: Flujo[] = [];
+
+  constructor(private sensorService: SensorService, private historicosService: HistoricosService) { }
 
   ngOnInit(): void {
+    this.llenarHumedad();
+    this.llenarTemperatura();
+    this.llenarFlujo();
+
     // Llama a la función para actualizar datos cada segundo
     setInterval(() => {
       this.humedadTiempoReal();
       this.temperaturatTiempoReal();
       this.flujoTiempoReal();
+
+      if (this.datoHumedadReal && this.listaHumedad.length > 0) {
+        this.listaHumedad.push({
+          ...this.listaHumedad[this.listaHumedad.length - 1],
+          valor: Number(this.datoHumedadReal),
+          fecha_hora: new Date().toISOString()
+        });
+        this.listaHumedad.shift();
+      }
+      if (this.datoTemperaturaReal && this.listaTemperatura.length > 0) {
+        this.listaTemperatura.push({
+          ...this.listaTemperatura[this.listaTemperatura.length - 1],
+          valor: Number(this.datoTemperaturaReal),
+          fecha_hora: new Date().toISOString()
+        });
+        this.listaTemperatura.shift();
+      }
+
     }, 1000); // cada segundo
   }
-  
+
+  llenarHumedad() {
+    // Simulación de llamada al endpoint
+    this.historicosService.getUltimasHumedades(10).subscribe((data: any[]) => {
+      this.listaHumedad = data;
+    });
+  }
+
+  llenarTemperatura() {
+    this.historicosService.getUltimasTemperaturas(10).subscribe((data: any[]) => {
+      this.listaTemperatura = data;
+    });
+  }
+
+  llenarFlujo() {
+    this.historicosService.getUltimosFlujos(10).subscribe((data: any[]) => {
+      this.listaFlujo = data;
+    });
+  }
 
   humedadTiempoReal() {
     this.sensorService.getHumedadActual().subscribe((data: any) => {
