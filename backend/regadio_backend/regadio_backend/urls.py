941021d054302b369f54a4e0de9e108df61e65e9
GudmarSQ <gudmarpa13@gmail.com>
2025-06-25 03:11:04 -0400

    Chat con IA

--- backend/regadio_backend/regadio_backend/urls.py
+++ backend/regadio_backend/regadio_backend/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-
 schema_view = get_schema_view(
    openapi.Info(
       title="API Regadío",
@@ -16,11 +15,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('riego_api.urls')),
-   #  path('test/', include('mongo_models.urls')),
+    # path('test/', include('mongo_models.urls')),
     path('login/', include('riego_api.urls')),
+    path('api/', include('riego_api.urls')),
     # path('my_maps/', include('my_maps.urls')),
-
+    # path('api/chat/', chat_with_gemini, name='chat_with_gemini'), 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
